51152384f58d2051bba0fb1897fa676bd7852fae
Maximilian Stock <me@maximumstock.net>
2017-01-27 14:27:26 +0100

    fix null values for integer jodel values

--- priv/repo/migrations/20170111153349_create_jodels.exs
+++ priv/repo/migrations/20170111153349_create_jodels.exs
@@ -5,12 +5,12 @@ defmodule Jodels.Repo.Migrations.CreateJodels do
     create table(:jodels, primary_key: false) do
       add :post_id, :string, size: 24, primary_key: true
       add :message, :text
-      add :pin_count, :integer, default: 0
+      add :pin_count, :integer, null: false, default: 0
       add :hex_color, :string, size: 6
-      add :distance, :integer, default: 0
-      add :child_count, :integer, default: 0
+      add :distance, :integer, null: false, default: 0
+      add :child_count, :integer, null: false, default: 0
       add :is_image, :boolean, default: false
-      add :vote_count, :integer, default: 0
+      add :vote_count, :integer, null: false, default: 0
       add :location_name, :text
       add :user_handle, :string, size: 45
       add :image_url, :text, default: nil
