bbddd88f3ddf632630fe727fdcfbf851f0604905
Maximilian Stock <me@maximumstock.net>
2017-04-27 13:12:02 +0200

    first try

---
+++ lib/jodel_scraper/evaluations/frequency_genstage/setup.ex
@@ -0,0 +1,36 @@
+defmodule JodelScraper.Evaluations.Frequency.Setup do
+
+
+  alias JodelScraper.Evaluations.Frequency.{Scraper, Processor}
+
+  require Logger
+
+  @intervals [30, 60, 120, 240, 600]
+
+  def start do
+
+    @intervals
+    |> Enum.map(fn interval ->
+        {:ok, producer} = Producer.start_test()
+        {:ok, consumer} = Consumer.start_link()
+        GenStage.sync_subscribe(consumer, to: producer)
+        {consumer, interval}
+    end)
+    |> Enum.each(fn {consumer, interval} ->
+      Process.send_after(self(), {:tick, consumer, interval}, interval*1000)
+    end)
+
+    loop()
+
+  end
+
+  def loop do
+    receive do
+      {:tick, consumer, interval} = msg ->
+        Consumer.ask(consumer)
+        Process.send_after(self(), msg, interval*1000)
+        loop()
+    end
+  end
+
+end
