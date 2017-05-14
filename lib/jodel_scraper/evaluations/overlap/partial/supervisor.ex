a047431b46d4d7a15a80e5ed091d53bc0cdbe545
Maximilian Stock <me@maximumstock.net>
2017-05-14 20:56:09 +0200

    partial overlap tests: add München as testing location; client: update api version

--- lib/jodel_scraper/evaluations/overlap/partial/supervisor.ex
+++ lib/jodel_scraper/evaluations/overlap/partial/supervisor.ex
@@ -13,29 +13,56 @@ defmodule Evaluations.Overlap.Partial.Supervisor do
 
   def init([]) do
 
-    locations = [%{
-      name: "Berlin Base",
-      lat: 52.522254,
-      lng: 13.226990,
-      id: 1
-    }, %{
-      name: "Berlin 2",
-      lat: 52.525596,
-      lng: 13.327240,
-      id: 2
-    }, %{
-      name: "Berlin 3",
-      lat: 52.528938,
-      lng: 13.443970,
-      id: 3
-    }, %{
-      name: "Berlin 4",
-      lat: 52.531445,
-      lng: 13.606018,
-      id: 4
-    }]
+		# locations = [%{
+    #   name: "Berlin Base",
+    #   lat: 52.522254,
+    #   lng: 13.226990,
+    #   id: 1
+    # }, %{
+    #   name: "Berlin 2",
+    #   lat: 52.525596,
+    #   lng: 13.327240,
+    #   id: 2
+    # }, %{
+    #   name: "Berlin 3",
+    #   lat: 52.528938,
+    #   lng: 13.443970,
+    #   id: 3
+    # }, %{
+    #   name: "Berlin 4",
+    #   lat: 52.531445,
+    #   lng: 13.606018,
+    #   id: 4
+    # }]
 
-    children =
+    locations = [%{
+        name: "München 1",
+        lat: 48.1485,
+        lng: 11.4629,
+        id: 1
+      }, %{
+        name: "München 2",
+        lat: 48.1485,
+        lng: 11.4973,
+        id: 2
+      }, %{
+        name: "München 3",
+        lat: 48.1485,
+        lng: 11.5296,
+        id: 3
+      }, %{
+        name: "München 4",
+        lat: 48.1485,
+        lng: 11.5620,
+        id: 4
+      }, %{
+        name: "München 5",
+        lat: 48.1485,
+        lng: 11.6020,
+        id: 5
+      }]
+
+		children =
       locations
       |> Enum.map(fn location ->
         %DynamicScraper{
